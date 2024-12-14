593406aaf6e260ff831945e6605b311c2f85c12c
David <davidlboyd1986@gmail.com>
2024-12-14 00:13:50 -0500

    Have not deployed: Added parameters.json instead of having them in build_script. Add  to build_script because I was using it in cicd where I didn't need it.

--- build_script.sh
+++ build_script.sh
@@ -1,39 +1,47 @@
+#!/bin/bash
 
 echo "Starting deployment...."
+
+# Get the directory where the script is located
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 # Deploy the Bastion Host VPC and Infrastructure Stacks
 echo "Deploy the BH Networking stack"
 aws cloudformation deploy --stack-name eks-hacking-bh-vpc-stack \
-    --template-file ./IaC_Templates/bastion_host_vpc_deployment.yml
+    --template-file $SCRIPT_DIR/IaC_Templates/bastion_host_vpc_deployment.yml
 
 echo "Deploy the BH Infrastructure stack - EC2 Instance with Security Groups"
 aws cloudformation deploy --stack-name eks-hacking-bh-infrastructure-stack \
-    --template-file ./IaC_Templates/bastion_host_infrastructure_deployment.yml \
-    --parameters ParameterKey=personalPublicIP,ParameterValue=192.168.0.0 \
-                 ParameterKey=userName,ParameterValue=BastionUser \
-                 ParameterKey=userPass,ParameterValue=ChangeMeNow! \
-                 ParameterKey=bastionHostPublicKey,ParameterValue=EnterPublicKeyHere \
+    --template-file $SCRIPT_DIR/IaC_Templates/bastion_host_infrastructure_deployment.yml \
+    --parameters file://$SCRIPT_DIR/parameter.json \
     --capabilities CAPABILITY_IAM
 
 # Deploys the EKS VPC and Infrastructure Stacks
 echo "Deploy the EKS Networking stack"
 aws cloudformation deploy --stack-name eks-hacking-eks-vpc-stack \
-    --template-file ./IaC_Templates/eks_vpc_deployment.yml
+    --template-file $SCRIPT_DIR/IaC_Templates/eks_vpc_deployment.yml
 
 echo "Deploy the EKS IAM stack"
-aws cloudformation deploy --stack-name eks-hacking-eks-iam-stack --template-file ./IaC_Templates/eks_iam_deployment.yml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --stack-name eks-hacking-eks-iam-stack \
+    --template-file $SCRIPT_DIR/IaC_Templates/eks_iam_deployment.yml \
+    --capabilities CAPABILITY_NAMED_IAM
 
 echo "Deploy the EKS Infrastructure Stack"
-aws cloudformation deploy --stack-name eks-hacking-eks-infrastructure-stack --template-file ./IaC_Templates/eks_infrastructure_deployment.yml
+aws cloudformation deploy --stack-name eks-hacking-eks-infrastructure-stack \
+    --template-file $SCRIPT_DIR/IaC_Templates/eks_infrastructure_deployment.yml
 
 # Must get Target Group ARN to use in Kubernetes Resource Deployments
 echo "Get target group ARN from previously deployed stack, to use in EKS Service"
-TARGET_GROUP_ARN=$(aws cloudformation describe-stacks --stack-name eks-hacking-eks-infrastructure-stack --query "Stacks[0].Outputs[?OutputKey=='EKSHackingALBTargetGroupARN'].OutputValue" --output text)
+TARGET_GROUP_ARN=$(aws cloudformation describe-stacks \
+                    --stack-name eks-hacking-eks-infrastructure-stack \
+                    --query "Stacks[0].Outputs[?OutputKey=='EKSHackingALBTargetGroupARN'].OutputValue" \
+                    --output text)
 echo $TARGET_GROUP_ARN
 
 # Deploy the VPC Peering Connection Stack - Used to connect BH and EKS VPCs
 echo "Deploy VPC Peering Connection so Bastion Hosts can connect to EKS Cluster"
-aws cloudformation deploy --stack-name eks-hacking-eks-bh-vpc-peering-stack --template-file ./IaC_Templates/eks_bh_vpc_peering_deployment.yml
+aws cloudformation deploy --stack-name eks-hacking-eks-bh-vpc-peering-stack \
+    --template-file $SCRIPT_DIR/IaC_Templates/eks_bh_vpc_peering_deployment.yml
 
 # Configure kubectl to connect to eks cluster.
 echo "Configuring kubectl to be able to communicate with cluster"
@@ -47,7 +55,8 @@ aws sts get-caller-identity
 # Run some test commands
 echo "Check nodes were deployed and show as such"
 kubectl get nodes
-aws eks describe-nodegroup --cluster-name EKSHackingNodeCluster --nodegroup-name EKSHackingNodeGroup
+aws eks describe-nodegroup --cluster-name EKSHackingNodeCluster \
+    --nodegroup-name EKSHackingNodeGroup
 
 echo "The only user access to the cluster is for the IAM User/Role that created the cluster."
 echo "If an authentication error appears, than you are using another IAM User/Role, and must add them to the EKS Cluster"
@@ -63,14 +72,14 @@ else
 fi
 
 echo "Apply the deployment manifest to your cluster/namespace...."
-kubectl apply -f ./kubernetes/vulnerable-web-app-deployment.yml
+kubectl apply -f $SCRIPT_DIR/kubernetes/vulnerable-web-app-deployment.yml
 
 # Need to create separate file to swap out TARGET_GROUP_ARN with the target group of the created ALB
-cp ./kubernetes/vulnerable-web-app-service-template.yml vulnerable-web-app-service.yml
+cp $SCRIPT_DIR/kubernetes/vulnerable-web-app-service-template.yml $SCRIPT_DIR/vulnerable-web-app-service.yml
 sed "s/{{TARGET_GROUP_ARN}}/$TARGET_GROUP_ARN/g" vulnerable-web-app-service.yml
 
 echo "Apply the service manifest to your cluster/namespace...."
-kubectl apply -f ./kubernetes/vulnerable-web-app-service.yml
+kubectl apply -f $SCRIPT_DIR/kubernetes/vulnerable-web-app-service.yml
 
 # Tests the application deployed successfully
 echo "View the created kubernetes resources"
